d4fbd011612e8201505d60572f6098d1ad3f42e2
Vaibhav2399 <vaibhavparihar38@gmail.com>
2023-12-08 19:03:51 -0500

    Index working, eng-french translation working

--- detail.js
+++ detail.js
@@ -1,26 +1,14 @@
-// Wait for the DOM to be ready
-document.addEventListener('DOMContentLoaded', async function () {
-    try {
-        // Retrieve the result from the URL
-        var urlParams = new URLSearchParams(window.location.search);
-        var result = urlParams.get('result');
-
-        // Display the result on the page
-        document.getElementById('detailResult').innerText = await getTranslatedResult(result);
-    } catch (error) {
-        console.error(error);
-        // Handle the error, e.g., display an error message
-        document.getElementById('detailResult').innerText = "Error: " + error;
-    }
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('searchBtn').addEventListener('click', searchAndNavigate);
+    document.getElementById('englishTerm').addEventListener('keypress', handleKeyPress);
 });
 
-// Function to fetch translated result
-async function getTranslatedResult(result) {
-    // Simulate an asynchronous operation, e.g., fetching additional details
-    return new Promise(resolve => {
-        setTimeout(() => {
-            // Resolve with the result (or perform additional processing)
-            resolve(result);
-        }, 1000); // Simulating a delay of 1 second (adjust as needed)
-    });
-}
+document.addEventListener('DOMContentLoaded', () => {
+    // Retrieve query parameters
+    const params = new URLSearchParams(window.location.search);
+    const term = params.get('term');
+    const translation = params.get('translation');
+
+    // Display the result on the page
+    document.getElementById('detailResult').innerText = `French Translation: ${translation}`;
+});
--- main.js
+++ main.js
@@ -1,3 +1,82 @@
+// main.js
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('searchBtn').addEventListener('click', searchAndNavigate);
+    document.getElementById('englishTerm').addEventListener('keypress', handleKeyPress);
+    document.getElementById('indexBtn').addEventListener('click', indexx);
+});
+
+
+function handleKeyPress(event) {
+    if (event.key === "Enter") {
+        searchAndNavigate();
+    }
+}
+
+function indexx() {
+    fetch('data/vocab_example.csv')
+        .then(response => response.text())
+        .then(csvData => {
+            const data = parseCSV(csvData);
+            displayIndex(data);
+        })
+        .catch(error => {
+            console.error('Error fetching CSV: ', error);
+        });
+}
+
+function displayIndex(data) {
+    const resultContainer = document.getElementById('result');
+    
+    // Clear previous content
+    resultContainer.innerHTML = '';
+
+    // Display the data on the page
+    data.forEach(row => {
+        const rowElement = document.createElement('h5');
+        rowElement.textContent = `English: ${row.English}, French: ${row.French}`;
+        resultContainer.appendChild(rowElement);
+    });
+}
+
+// Function to read and translate from CSV
+function translations(englishTerm) {
+    return new Promise((resolve, reject) => {
+        // Fetch the CSV file content
+        fetch('data/vocab_example.csv')
+            .then(response => response.text())
+            .then(csvData => {
+                const data = parseCSV(csvData);
+                const translation = data.find(row => row.English === englishTerm);
+                // multisensory information
+                if (translation) {
+                    resolve(translation.French);
+                } else {
+                    reject("Translation not found.");
+                }
+            })
+            .catch(error => {
+                reject('Error fetching CSV: ' + error);
+            });
+    });
+}
+
+async function searchAndNavigate() {
+    try {
+        var englishTerm = document.getElementById('englishTerm').value;
+        // document.getElementById('result').innerText = 'Result: ' + englishTerm;
+
+        var translationsResult = await translations(englishTerm);
+        // Redirect to detail.html with the translation result as a query parameter
+        window.location.href = `detail.html?term=${encodeURIComponent(englishTerm)}&translation=${encodeURIComponent(translationsResult)}`;
+        // document.getElementById('result2').innerText = 'Result2: ' + translationsResult;
+    } catch (error) {
+        console.error(error);
+        document.getElementById('result').innerText = 'Error: ' + error;
+    }
+}
+
+
 function parseCSV(csv) {
     const lines = csv.split('\n');
     const headers = lines[0].split(',');
@@ -16,52 +95,3 @@ function parseCSV(csv) {
 
     return data;
 }
-
-async function searchAndNavigate() {
-    try {
-        // Get the entered English term
-        var englishTerm = document.getElementById('englishTerm').value;
-
-        // Call the translations function and pass the term
-        var translationsResult = await translations(englishTerm);
-
-        // Navigate to detail.html and pass the result as a query parameter
-        window.location.href = 'detail.html?result=' + encodeURIComponent(translationsResult);
-    } catch (error) {
-        console.error(error);
-        // Handle the error, e.g., display an error message
-        alert("Error: " + error);
-    }
-}
-
-
-function handleKeyPress(event) {
-    // Check if the pressed key is "Enter" (key code 13)
-    if (event.key === "Enter") {
-        // Call the translations function when Enter is pressed
-        searchAndNavigate();
-    }
-}
-
-// Function to read and translate from CSV
-function translations(englishTerm) {
-    return new Promise((resolve, reject) => {
-        // Fetch the CSV file content
-        fetch('data/vocab_example.csv')
-            .then(response => response.text())
-            .then(csvData => {
-                const data = parseCSV(csvData);
-                const translation = data.find(row => row.English === englishTerm);
-
-                if (translation) {
-                    resolve(translation.French);
-                } else {
-                    reject("Translation not found.");
-                }
-            })
-            .catch(error => {
-                reject('Error fetching CSV: ' + error);
-            });
-    });
-}
-
