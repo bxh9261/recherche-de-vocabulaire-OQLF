e69f90811c7b07c7a8385bebc8a0c2bf00797be6
Vaibhav2399 <vaibhavparihar38@gmail.com>
2023-12-18 01:42:22 -0500

    Few minor changes

--- main.js
+++ main.js
@@ -25,41 +25,31 @@ function indexx() {
 function displayIndex(data) {
     const resultContainer = document.getElementById('result');
 
-    // Clear previous content
     resultContainer.innerHTML = '';
 
-    // Sort the data alphabetically by the "English" property
     data.sort((a, b) => a.English.localeCompare(b.English));
 
-    // Display the data in a table
     const tableElement = document.createElement('table');
     tableElement.classList.add('index-table');
 
-    // Create table header
     const headerRow = tableElement.insertRow();
     const headerCell1 = headerRow.insertCell(0);
     const headerCell2 = headerRow.insertCell(1);
     headerCell1.textContent = 'English';
     headerCell2.textContent = 'French';
 
-    // Add a class to the header cells for styling (optional)
     headerCell1.classList.add('header-cell');
     headerCell2.classList.add('header-cell');
 
-    // Variable to store the previous first letter
     let previousFirstLetter = '';
 
-    // Create table rows
     data.forEach(row => {
-        // Check if the first letter is different from the previous entry
         if (row.English.charAt(0).toUpperCase() !== previousFirstLetter) {
-            // Add a line break before this entry
             const lineBreakRow = tableElement.insertRow();
             const lineBreakCell1 = lineBreakRow.insertCell(0);
             const lineBreakCell2 = lineBreakRow.insertCell(1);
             lineBreakCell1.classList.add('line-break');
             lineBreakCell2.classList.add('line-break');
-            // Update the previous first letter
             previousFirstLetter = row.English.charAt(0).toUpperCase();
         }
 
@@ -69,22 +59,18 @@ function displayIndex(data) {
         cell1.textContent = row.English;
         cell2.textContent = row.French;
 
-        // Add a class to the data cells for styling
         cell1.classList.add('data-cell');
         cell2.classList.add('data-cell');
     });
 
-    // Append the table to the result container
     resultContainer.appendChild(tableElement);
 }
 
 
 
 
-// Function to read and translate from CSV
 function translations(englishTerm) {
     return new Promise((resolve, reject) => {
-        // Fetch the CSV file content
         fetch('data/vocab_example.csv')
             .then(response => response.text())
             .then(csvData => {
